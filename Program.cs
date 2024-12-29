65e245f3adcaa8f9e4ac7a7af6b016cabe7a3092
mr3bzo <eabdelrahman18@gmail.com>
2024-12-29 13:19:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,192 @@
+﻿using Common;
+using Enums;
+namespace Demo01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exception Handling
+            //DoSomeCode();
+
+            //Exceptions
+            // 1. System.Exception
+            // 1.1 format exception
+            // 1.2 index out of range exception
+            // 1.3 null reference exception
+            // 1.4 invalid operation exception
+            // 1.5 Arithmetic exception
+            // 1.5.1 divide by zero exception
+            // 1.5.2 overflow exception
+            // 2. Application exception
+            //try
+            //{
+            //    DoSomeCodeProtictive();
+            //    //throw new Exception();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    // Close - free - delete- deallocate unmanged resources
+            //    //Open File
+            //    //Open Connection Database
+            //    Console.WriteLine("Finally"); //Exception or not will run
+            //}
+            //// DoSomeCodeProtictive();
+            //// throw new Exception();
+            //Console.WriteLine("After Try Catch");
+
+            #endregion
+
+            #region Access Modifier
+            //TypeA typeA = new TypeA();
+            //TypeB typeB = new TypeB(); 
+            #endregion
+
+            #region Enum
+            #region EX01
+            //string Day = "Hamo";
+            //Days day = Days.Fri;
+            //Grades grade = Grades.A;
+            //switch (grade)
+            //{
+            //    case Grades.A:
+            //        Console.WriteLine("Excellent");
+            //        break;
+            //    case Grades.B:
+            //        Console.WriteLine("Very Good");
+            //        break;
+            //    case Grades.C:
+            //        Console.WriteLine("Good");
+            //        break;
+            //    case Grades.D:
+            //        Console.WriteLine("Pass");
+            //        break;
+            //    case Grades.F:
+            //        Console.WriteLine("Fail");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid Grade");
+            //        break;
+            //}
+            #endregion
+
+            #region EX02
+            //Grades grades = (Grades)2;
+            //Grades grades = (Grades)10;
+            //Console.WriteLine(grades);
+            #endregion
+
+            #region Ex03
+            //Grades grades = (Grades)Enum.Parse(typeof(Grades),Console.ReadLine());
+            //Console.WriteLine(grades);
+            //Enum.TryParse(typeof(Grades), Console.ReadLine(), out object result);
+            //Grades grade = (Grades)result;
+            //Console.WriteLine(result);
+            #endregion
+
+            #region EX04
+            //string gender = "male";
+            ////Enum.TryParse(typeof(Gender), gender, out object result);
+            ////Console.WriteLine(result);
+
+            //Enum.TryParse<Gender>(gender,true,out Gender result);
+            //Console.WriteLine(result);
+
+            //Gender gender = new Gender();
+            //Console.WriteLine(gender);  //Male becuase default value = 0 and male = 0
+            #endregion
+
+
+            #endregion
+
+            #region Permissions
+            Employee employee = new Employee();
+            employee.name = "Ali";
+            employee.age = 20;
+            employee.Permissions = (Permissions)3;
+
+            Console.WriteLine(employee.Permissions); //Execute , Delete
+
+            //XOR
+
+            employee.Permissions = employee.Permissions ^ Permissions.read;
+
+            Console.WriteLine(employee.Permissions); //delete ,execute ,read
+
+            employee.Permissions = employee.Permissions ^ Permissions.read;
+
+            Console.WriteLine(employee.Permissions); //delete , execute
+
+            //&
+            if ((employee.Permissions & Permissions.read) == Permissions.read)
+            {
+                Console.WriteLine("read is exist");
+            }
+            else
+            {
+                employee.Permissions = employee.Permissions ^ Permissions.read;
+            }
+            Console.WriteLine(employee.Permissions);
+
+            //OR
+            employee.Permissions = employee.Permissions | Permissions.write;
+            Console.WriteLine(employee.Permissions);
+            #endregion
+        }
+        public static void DoSomeCode()
+        {
+            try
+            {
+                int x, y, z;
+                x = int.Parse(Console.ReadLine()); //FormatException
+                y = int.Parse(Console.ReadLine());
+
+                z = x / y; //DivideByZeroException
+
+                int[] Numbers = { 1, 2, 3 };
+
+                Numbers[100] = 77; //IndexOutOfRangeException
+
+            }
+            //ClR Create Object From EXception
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                // Log In File
+                // Store in Database
+            }
+        }
+        public static void DoSomeCodeProtictive()
+        {
+            int x, y, z;
+            bool flag;
+            do
+            {
+                Console.Write("Enter X : ");
+                flag = int.TryParse(Console.ReadLine(), out x);
+
+            }while (!flag);
+
+            do
+            {
+                Console.Write("Enter Y : ");
+                flag = int.TryParse(Console.ReadLine(), out y);
+
+            } while (!flag | y == 0);
+
+            z= x / y;
+
+            int[] Numbers = { 1, 2, 3 };
+
+            if(Numbers?.Length > 100)
+            {
+                Numbers[100] = 77;
+            }
+
+        }
+    }
+}
---
+++ oop 01.cs
@@ -0,0 +1,176 @@
+﻿using System.Drawing;
+
+namespace assignment_01 {
+    class program
+    {
+        static void mian(string[]args) {
+
+
+            #region 01
+
+            //Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+
+            //  enum WeekDays
+            //{
+            //    Monday,
+            //    Tuesday,
+            //    Wednesday,
+            //    Thursday,
+            //    Friday,
+            //    Saturday,
+            //    Sunday
+            //}
+
+            //static void Main()
+            //{
+            //    Console.WriteLine("Days of the week:");
+
+            //    Array days = Enum.GetValues(typeof(WeekDays));
+            //    for (int i = 0; i < days.Length; i++)
+            //    {
+            //        Console.WriteLine(days.GetValue(i));   
+
+            #endregion
+
+            #region 02
+
+            //Create an enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+
+            //enum Season
+            //{
+            //    Spring,
+            //    Summer,
+            //    Autumn,
+            //    Winter
+            //}
+
+            //static void Main()
+            //{
+            //    Console.Write("Enter a season: ");
+            //    string userInput = Console.ReadLine();
+
+            //    if (Enum.TryParse(userInput, true, out Season season))
+            //    {
+            //        if (season == Season.Spring)
+            //        {
+            //            Console.WriteLine(" March to May");
+            //        }
+            //        else if (season == Season.Summer)
+            //        {
+            //            Console.WriteLine(" June to August");
+            //        }
+            //        else if (season == Season.Autumn)
+            //        {
+            //            Console.WriteLine("September to November");
+            //        }
+            //        else if (season == Season.Winter)
+            //        {
+            //            Console.WriteLine(" December to February");
+            //        }
+            //    }
+            //
+            #endregion
+
+
+            #region 03
+
+            //Assign the following Permissions (Read, write, Delete, Execute) in a form of Enum.
+            //Create Variable from previous Enum to Add and Remove Permission from variable, check if specific Permission is existed inside variable
+
+            //        [Flags]
+            //enum Permissions
+            //    {
+            //        Read = 1,
+            //        Write = 2,
+            //        Delete = 4,
+            //        Execute = 8
+            //    }
+
+            //    static void Main()
+            //    {
+            //        Permissions userPerms = Permissions.Read | Permissions.Write | Permissions.Execute;
+
+            //        if ((userPerms & Permissions.Write) == Permissions.Write)
+            //        {
+            //            Console.WriteLine("Write permission exists");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Write permission does not exist");
+            //        }
+
+            //        userPerms = userPerms ^ Permissions.Write;
+            //        Console.WriteLine(userPerms);
+
+            //        userPerms = userPerms | Permissions.Execute;
+            //        Console.WriteLine(userPerms);
+            //    }
+
+
+
+
+
+            #endregion
+
+
+            #region 04
+            //Create an enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
+
+
+            // enum Colors
+            //{
+            //    Red,
+            //    Green,
+            //    Blue
+            //}
+
+            //static void Main()
+            //{
+            //    Console.Write("Enter a color (Red/Green/Blue): ");
+            //    string userInput = Console.ReadLine();
+
+            //    if (Enum.TryParse(userInput, true, out Colors color))
+            //    {
+            //        // All colors in this enum are primary colors
+            //        Console.WriteLine($"{color} is a primary color!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("That's not a valid color in our list!");
+            //    }
+            //} 
+
+            #endregion
+
+            #region 05
+            // Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+
+
+            // Point point01;
+            //Console.WriteLine("Enter the coordinates for the first point : ");
+            //Console.Write($"Enter X : ");
+            //point01.X = int.Parse(Console.ReadLine());
+            //Console.Write($"Enter Y : ");
+            //point01.Y = int.Parse(Console.ReadLine());
+
+            //Point point02;
+            //Console.WriteLine("Enter the coordinates for the Second point : ");
+            //Console.Write($"Enter X : ");
+            //point02.X = int.Parse(Console.ReadLine());
+            //Console.Write($"Enter Y : ");
+            //point02.Y = int.Parse(Console.ReadLine());
+
+            //double Distance = Math.Sqrt(Math.Pow((point02.X - point01.X), 2) + Math.Pow((point02.Y - point01.Y), 2));
+            //Console.WriteLine($"The distance between the two points is : {Distance}");
+
+            #endregion
+
+
+
+
+
+
+
+        }
+    }
+}
